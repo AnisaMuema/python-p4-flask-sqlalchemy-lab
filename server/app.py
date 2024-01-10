47bfa019d72270ac80a139f593c727636fe7c118
anisamuema <anisa.muema.school@gmail.com>
2024-01-10 13:26:30 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -19,16 +19,49 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    response_body = f'''
+        <ul>Id: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species} </ul>
+        <ul>Zookeeper: {animal.zookeeper_id}</ul>
+        <ul>Enclosure: {animal.enclosure_id}</ul>
+    '''
+    response = make_response(response_body, 200)
+    
+    return response    
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    response_body = f'''
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+       
+    '''
+    animals=[animal for animal in zookeeper.animals]
+    for animal in animals:
+        response_body+= f'Animal: {animal.name}'
+
+    response = make_response(response_body, 200)
+    return response
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body = f'''
+        <ul>Environment: {enclosure.environment}
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+    '''
+    animals=[animal for animal in enclosure.animals]
+   
+    for animal in animals:
+        response_body +=f'<ul>Animal: {animal.name}</u>'
 
+    response = make_response(response_body, 200)
+    return response
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -7,17 +8,36 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class Zookeeper(db.Model):
+class Zookeeper(db.Model, SerializerMixin):
     __tablename__ = 'zookeepers'
-
+    serialize_rules = ('-animals.zookeeper',)
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
 
-class Enclosure(db.Model):
+    animals = db.relationship('Animal', backref='zookeeper')
+
+class Enclosure(db.Model, SerializerMixin):
     __tablename__ = 'enclosures'
-
+    serialize_rules = ('-animals.enclosure',)
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 
-class Animal(db.Model):
+    animals = db.relationship('Animal', backref='enclosure')
+
+    def __repr__(self):
+        return f'Enclosure {self.environment}'
+
+class Animal(db.Model, SerializerMixin):
     __tablename__ = 'animals'
-
+    serialize_rules = ('-enclosure.animals', '-zookeeper.animals')
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    def __repr__(self):
+        return f'Animal {self.name}'
